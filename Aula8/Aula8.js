da8c31f53ef5e00b460bd6c17965df76d1933127
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-09-08 10:51:15 -0300

    ninja

--- Aula8/Aula8.js
+++ Aula8/Aula8.js
@@ -17,3 +17,37 @@ function person () {
 
 console.log(person().nome)
 console.log(person().lastName)
+
+//Programação Funcional - a programação funcional enfatiza a aplicação de funções, tratando-as como valores de primeira classe.
+function adder (x) {
+
+    return function (y) {
+
+        return  x + y 
+
+    }
+}
+console.log(adder(5) (5))
+console.log('******************')
+let car = { 
+    color: 'yellow'
+
+}
+ 
+function getCarColor(mycar) {
+
+    return mycar.color;
+}
+
+console.log(getCarColor(car))
+
+function showOtherFunction (func) {
+
+    return func()
+}
+
+console.log(showOtherFunction(function() {
+
+        return ' Mudança de função '
+
+}))
